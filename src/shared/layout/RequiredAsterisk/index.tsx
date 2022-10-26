79f205a8ca6484a07cee6ed1dd6a98174c322a07
Pham Hung <hung.pham.duc@rits.center>
2022-10-26 17:36:48 +0700

    Revert "test"
    This reverts commit fefbd0d3413333cc6bec4faa5756abba53791b64.
    
    # Conflicts:
    #	package-lock.json
    #	src/App.tsx
    #	tsconfig.json

--- src/shared/layout/RequiredAsterisk/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface IRequiredAsterisk {
-  className?: string;
-}
-
-const RequiredAsterisk = ({ className }: IRequiredAsterisk) => <span className={`required-asterisk ${className}`}>*</span>;
-
-export default RequiredAsterisk;
